e3db4c4d0429ec59e9d5956c256147fac448088c
Rydenko Denys <d.rudenko@expoplatform.com>
2021-12-25 12:24:25 +0200

    added code

--- src/App.js
+++ src/App.js
@@ -2,12 +2,49 @@ import React, { Component } from 'react'
 import SecondComponent from './SecondComponent'
 
 class App extends Component {
+  //the first varian of create state in class
+  // constructor() {
+  //   super()
+  //   this.state = {
+  //     name: 'John',
+  //     lastName: 'Weak'
+  //   }
+  // }
+  //the second variant to create state in class
+  state = {
+    userName: 'Test user name',
+    count: 0
+  }
+  handleChangeUserName = (e) => {
+    //please don't do that!!!!
+    //this.state.userName = 'some value'
+
+    //correct way to change state of react component
+    this.setState({ userName: e.target.value })
+  }
+  increaseCountValue = () => {
+    this.setState((state, props) => ({
+      count: state.count + 1
+    }))
+  }
+  handleClickIncreaseCount = () => {
+    this.increaseCountValue()
+    this.increaseCountValue()
+    this.increaseCountValue()
+  }
   render() {
     return (
       <div>
         <h1>Hello React class component</h1>
-        <SecondComponent />
-      </div>
+        <input label='Enter user name'
+          defaultValue={this.state.userName}
+          onChange={this.handleChangeUserName} />
+        <br />
+        <button onClick={this.handleClickIncreaseCount}>
+          Increase count
+        </button>
+        <SecondComponent userName={this.state.userName} />
+      </div >
     )
   }
 }
--- src/SecondComponent.js
+++ src/SecondComponent.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-const SecondComponent = () => {
+const SecondComponent = ({ userName }) => {
     const [count, setCount] = useState(0)
     const handleClick = () => {
         setCount(count + 1)
@@ -8,6 +8,9 @@ const SecondComponent = () => {
     return (
         <div>
             <h1>Functional component</h1>
+            <p>
+                {userName}
+            </p>
             <span>Click count {count}</span>
             <br />
             <button onClick={handleClick}>Click me please</button>

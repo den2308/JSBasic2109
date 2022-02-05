16536e79aa7e47bf482d8c61f8f8abcfd92892af
Denis <den2308@gmail.com>
2022-02-05 12:51:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Routes, Route, Link } from "react-router-dom";
 import { PageHeader } from 'antd';
+import { useDispatch, useStore } from 'react-redux'
 
 import SecondComponent from './SecondComponent'
 import ThirdComponent from './ThirdComponent'
 
 const App = () => {
-
+  const dispatch = useDispatch()
+  const store = useStore()
+  const handleIncrement = () => {
+    dispatch({ type: 'INCREMENT' })
+    console.log('store', store.getState())
+  }
+  const handleDecrement = () => {
+    dispatch({ type: 'DECREMENT' })
+  }
   return (
     <div >
       <PageHeader
@@ -16,6 +25,8 @@ const App = () => {
         extra={[
           <Link key="home" to="/">Home</Link>,
           <Link key="nexcomponent" to="/next-component">Second component</Link>,
+          <button onClick={handleIncrement}>Increment</button>,
+          <button onClick={handleDecrement}>Decrement</button>,
         ]}
       />
       <Routes>

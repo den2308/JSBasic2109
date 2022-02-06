487b0a70177f2ea4582bde928ffc5701db7fb6ca
Denis <den2308@gmail.com>
2022-02-06 12:22:24 +0200

    Update reducers.js

--- src/store/reducers/reducers.js
+++ src/store/reducers/reducers.js
@@ -2,17 +2,23 @@
 import constants from '../constants/constants';
 
 const initialState = () => ({
-    count: 0
+    count: 0,
+    weather: {}
 })
 
 export default (state = initialState(), action) => {
     switch (action.type) {
         case constants.INCREMENT:
             console.log(action)
-            return { count: state.count + 1 }
+            return { ...state, count: state.count + 1 }
         case constants.DECREMENT:
             console.log(action)
-            return { count: state.count - 1 }
+            return { ...state, count: state.count - 1 }
+        case constants.GET_WEATHER_DATA:
+            return {
+                ...state,
+                weather: action.data
+            }
         default: return state
     }
 }
